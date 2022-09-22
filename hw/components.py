cfb4ad663b92a4b11b61bbf26aa4cb3f3b1bf90b
Rafael Corsi <corsiferrao@gmail.com>
2022-09-22 16:47:43 -0300

    solucao C componentes e ula

--- hw/components.py
+++ hw/components.py
@@ -1,233 +1,160 @@
 #!/usr/bin/env python3
 
-from myhdl import *
+from myhdl import block, always_comb
 
 
 @block
-def and16(a, b, q):
-    """
-    a: 16 bits
-    b: 16 bits
-    q: 16 bits
-
-    and bit a bit entre a e b
-    """
-    foo = Signal(0)
-
+def and16(q, a, b):
     @always_comb
     def comb():
-        q.next = foo
+        q.next = a[:] & b[:]
 
     return comb
 
 
 @block
 def or8way(a, b, c, d, e, f, g, h, q):
-    """
-    a, b, c, ... h: 1 bit
-
-    or bit a bit entre a e b
-    """
-    foo = Signal(intbv(0))
-
     @always_comb
     def comb():
-        q.next = foo
+        q.next = a | b | c | d | e | f | g | h
 
     return comb
 
 
 @block
 def orNway(a, q):
-    """
-    a: 16 bits
-    q: 1 bit
-
-    or bit a bit dos valores de a: a[0] or a[1] ...
-    """
-    foo = Signal(intbv(0))
-
     @always_comb
     def comb():
-        q.next = foo
+        t = 0
+        for i in range(0, a.max):
+            t = a[i] | t
+        q.next = t
 
     return comb
 
 
 @block
 def barrelShifter(a, dir, size, q):
-    """
-    a: 16 bits
-    dir: 1 bit
-    size: n bits
-    q: 16 bits
-
-    se dir for 0, shifta para direita `size`
-    se dir for 1, shifta para esquerda `size`
-
-    exemplo: a = 0000 1111 0101 1010, dir = 0, size = 3
-             q = 0111 1010 1101 0000
-    """
-    foo = Signal(intbv(0))
-
     @always_comb
     def comb():
-        q.next = foo
+        if dir:
+            q.next = a << size
+        else:
+            q.next = a >> size
 
     return comb
 
 
 @block
-def mux2way(q, a, b, sel):
-    """
-    q: 16 bits
-    a: 16 bits
-    b: 16 bits
-    sel: 2 bits
-
-    Mux entre a e b, sel é o seletor
-    """
-    foo = Signal(intbv(0))
-
+def mux2Way(q, a, b, sel):
     @always_comb
     def comb():
-        q.next = foo
+        if sel == 0:
+            q.next = a
+        else:
+            q.next = b
 
     return comb
 
 
 @block
-def mux4way(q, a, b, c, d, sel):
-    """
-    q: 16 bits
-    a: 16 bits
-    b: 16 bits
-    c: 16 bits
-    d: 16 bits
-    sel: 4 bits
-
-    Mux entre a, b, c, d sel é o seletor
-    """
-    foo = Signal(intbv(0))
-
+def mux4Way(q, a, b, c, d, sel):
     @always_comb
     def comb():
-        q.next = foo
+        if sel == 0:
+            q.next = a
+        elif sel == 1:
+            q.next = b
+        elif sel == 2:
+            q.next = c
+        else:
+            q.next = d
 
     return comb
 
 
 @block
-def mux8way(q, a, b, c, d, e, f, g, h, sel):
-    """
-    Mux de 8 entradas, simular aos anteriores.
-    """
-
-    foo = Signal(intbv(0))
-
+def mux8Way(q, a, b, c, d, e, f, g, h, sel):
     @always_comb
     def comb():
-        q.next = foo
+        inMux = [a, b, c, d, e, f, g, h]
+        q.next = inMux[sel]
 
     return comb
 
 
 @block
-def deMux2way(a, q0, q1, sel):
-    """
-    deMux de 2 saídas e uma entrada.
-
-    - Lembre que a saída que não está ativada é 0
-
-    Exemplo:
-
-    a = 0xFFAA, sel = 0
-    q0 = 0xFFAA
-    q1 = 0
-    """
-
-    foo = Signal(intbv(0))
-
+def mux(q, signals, sel):
     @always_comb
     def comb():
-        q0.next = foo
+        q.next = signals[sel]
 
     return comb
 
 
 @block
-def deMux4way(a, q0, q1, q2, q3, sel):
-    """
-    deMux de 4 saídas e uma entrada.
-
-    - Lembre que a saída que não está ativada é 0
-    """
-
-    foo = Signal(intbv(0))
-
+def deMux2Way(a, q0, q1, sel):
     @always_comb
     def comb():
-        q0.next = foo
+        if sel == 0:
+            q0.next = a
+            q1.next = 0
+        else:
+            q0.next = 0
+            q1.next = a
 
     return comb
 
 
 @block
-def deMux8way(a, q0, q1, q2, q3, q4, q5, q6, q7, sel):
-    """
-    deMux de 8 saídas e uma entrada.
-
-    - Lembre que a saída que não está ativada é 0
-    """
-
-    foo = Signal(intbv(0))
-
+def deMux4Way(a, q0, q1, q2, q3, sel):
     @always_comb
     def comb():
-        q0.next = foo
+        q0.next = 0
+        q1.next = 0
+        q2.next = 0
+        q3.next = 0
 
-    return comb
-
-
-# -----------------------------#
-# Conceito B
-# -----------------------------#
-#
-@block
-def bin2hex(hex0, sw):
-    """
-    importar do lab!
-    """
-
-    @always_comb
-    def comb():
-        hex0.next[4:] = sw[4:]
+        if sel == 0:
+            q0.next = a
+        elif sel == 1:
+            q1.next = a
+        elif sel == 2:
+            q2.next = a
+        else:
+            q3.next = a
 
     return comb
 
 
 @block
-def bin2bcd(b, bcd1, bcd0):
-    """
-    componente que converte um vetor de b[8:] (bin)
-    para dois digitos em BCD
-
-    Exemplo:
-    bin  = `01010010`
-    BCD1 = 8
-    BCD0 = 2
-    """
-
-    foo = Signal(intbv(0)[4:])
-
+def deMux8Way(a, q0, q1, q2, q3, q4, q5, q6, q7, sel):
     @always_comb
     def comb():
-        bcd1.next = foo
-        bcd0.next = foo
+        q0.next = 0
+        q1.next = 0
+        q2.next = 0
+        q3.next = 0
+        q4.next = 0
+        q5.next = 0
+        q6.next = 0
+        q7.next = 0
+
+        if sel == 0:
+            q0.next = a
+        elif sel == 1:
+            q1.next = a
+        elif sel == 2:
+            q2.next = a
+        elif sel == 3:
+            q3.next = a
+        elif sel == 4:
+            q4.next = a
+        elif sel == 5:
+            q5.next = a
+        elif sel == 6:
+            q6.next = a
+        else:
+            q7.next = a
 
     return comb
-
-
-# -----------------------------#
-# Conceito A
-# -----------------------------#
--- hw/ula.py
+++ hw/ula.py
@@ -22,9 +22,26 @@ def ula(x, y, c, zr, ng, saida, width=16):
     c_f = c(1)
     c_no = c(0)
 
+    zx = zerador(c_zx, x, zx_out)
+    nx = inversor(c_nx, zx_out, nx_out)
+
+    zy = zerador(c_zy, y, zy_out)
+    ny = inversor(c_ny, zy_out, ny_out)
+
+    add = add16(nx_out, ny_out, add_out)
+
+    no = inversor(c_no, mux_out, no_out)
+    comp = comparador(no_out, zr, ng, width)
+
     @always_comb
     def comb():
-        pass
+        # aqui poderia usar o mux2way!
+        if c_f == 1:
+            mux_out.next = add_out
+        else:
+            mux_out.next = nx_out & ny_out
+
+        saida.next = no_out[len(saida) :]
 
     return instances()
 
@@ -35,112 +52,104 @@ def ula(x, y, c, zr, ng, saida, width=16):
 def inversor(z, a, y):
     @always_comb
     def comb():
-        pass
+        if z == 1:
+            y.next = ~a
+        else:
+            y.next = a
 
-    return instances()
+    return comb
 
 
 @block
 def comparador(a, zr, ng, width):
-    # width insica o tamanho do vetor a
     @always_comb
     def comb():
-        pass
+        ng.next = 0
+        zr.next = 0
+        if int(a[16:].signed()) < 0:
+            ng.next = 1
+            zr.next = 0
+        if a == 0:
+            zr.next = 1
+            ng.next = 0
 
-    return instances()
+    return comb
 
 
 @block
 def zerador(z, a, y):
     @always_comb
     def comb():
-        pass
+        if z == 1:
+            y.next = 0
+        else:
+            y.next = a
 
-    return instances()
+    return comb
 
 
 @block
 def add(a, b, q):
     @always_comb
     def comb():
-        pass
+        q.next = a + b
 
-    return instances()
-
-
-@block
-def inc(a, q):
-    @always_comb
-    def comb():
-        pass
-
-    return instances()
-
-
-# ----------------------------------------------
-# Conceito B
-# ----------------------------------------------
+    return comb
 
 
 @block
-def halfAdder(a, b, soma, carry):
-    s = Signal(bool())
-    c = Signal(bool())
+def inc16(a, q):
+    one = Signal(intbv(1))
 
-    @always_comb
-    def comb():
-        s = a ^ b
-        c = a & b
+    add16_1 = add16(a, one, q)
 
-        soma.next = s
-        carry.next = c
-
-    return instances()
+    return add16_1
 
 
 @block
-def fullAdder(a, b, c, soma, carry):
-    s = [Signal(bool(0)) for i in range(3)]
-    haList = [None for i in range(2)]
+def add16(a, b, q, n=16):
+    # nao podemos acessar direto um intbv como
+    # se fosse um sinal. Devemos criar um shadow signal
+    # que eh apenas readonly!
+    _a = [a(i) for i in range(n)]
+    _b = [b(i) for i in range(n)]
+    _c = [Signal(bool(0)) for i in range(n + 1)]
+    _q = [Signal(bool(0)) for i in range(n)]
 
-    haList[0] = halfAdder(a, b, s[0], s[1])  # 2
-    haList[1] = halfAdder(c, s[0], soma, s[2])  # 3
+    out_q = ConcatSignal(*reversed(_q))
+
+    fa_list = [None for i in range(n)]
+    for i in range(len(fa_list)):
+        fa_list[i] = fullAdder(_a[i], _b[i], _c[i], _q[i], _c[i + 1])
 
     @always_comb
     def comb():
-        carry.next = s[1] | s[2]  # 4
+        q.next = out_q
 
     return instances()
 
 
 @block
-def addcla4(a, b, q):
+def fullAdder(a, b, c, q, carry):
+    s1 = Signal(bool(0))
+    s2 = Signal(bool(0))
+    s3 = Signal(bool(0))
+
+    half_1 = halfAdder(a, b, s1, s2)
+    half_2 = halfAdder(c, s1, q, s3)
+
     @always_comb
     def comb():
-        pass
+        carry.next = s2 | s3
 
     return instances()
 
 
 @block
-def addcla16(a, b, q):
+def halfAdder(a, b, q, carry):
     @always_comb
     def comb():
-        pass
+        carry.next = a & b
+        q.next = a ^ b
 
-    return instances()
-
-
-# ----------------------------------------------
-# Conceito A
-# ----------------------------------------------
-
-
-@block
-def ula_new(x, y, c, zr, ng, sr, sf, bcd, saida, width=16):
-    pass
-
-
-@block
-def bcdAdder(x, y, z):
-    pass
+    return comb
